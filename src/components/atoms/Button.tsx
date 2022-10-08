0b55e352b7b1915ebbde5343a4aeea5784f78860
Alessio Sferro <sferro.alessio@gmail.com>
2022-10-08 07:31:14 +0200

    fix: button

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -4,6 +4,6 @@ interface ButtonProps extends ComponentProps<"button"> {
   text: string;
 }
 
-export const Button: FC<ButtonProps> = ({ text }) => {
-  return <button>{text}</button>;
+export const Button: FC<ButtonProps> = ({ text, ...props }) => {
+  return <button {...props}>{text}</button>;
 };
